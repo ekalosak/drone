6a044fe9052026621fb037c80af7a06c16298274
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2017-12-12 02:25:50 -0800

    rendering a cube inside another

--- opengl_test.py
+++ opengl_test.py
@@ -16,6 +16,17 @@ vertices= (
         (-1, 1, 1)
     )
 
+vertices2= (
+        (1.2, -1.2, -1.2),
+        (1.2, 1.2, -1.2),
+        (-1.2, 1.2, -1.2),
+        (-1.2, -1.2, -1.2),
+        (1.2, -1.2, 1.2),
+        (1.2, 1.2, 1.2),
+        (-1.2, -1.2, 1.2),
+        (-1.2, 1.2, 1.2)
+    )
+
 edges = (
         (0,1),
         (0,3),
@@ -76,6 +87,25 @@ def Cube():
             glVertex3fv(vertices[vertex])
     glEnd()
 
+def Cube2():
+    glBegin(GL_QUADS)
+    x = 0
+    for surface in surfaces:
+        # x = 0
+        for vertex in surface:
+            # x+=1
+            glColor3fv(colors[x])
+            glVertex3fv(vertices2[vertex])
+    glEnd()
+
+    glBegin(GL_LINES)
+    x = 3
+    for edge in edges:
+        for vertex in edge:
+            glColor3fv(colors[x])
+            glVertex3fv(vertices2[vertex])
+    glEnd()
+
 def main():
     pygame.init()
     display = (800,600)
@@ -91,21 +121,9 @@ def main():
                 pygame.quit()
                 quit()
 
-        if event.type == pygame.KEYDOWN:
-            # pygame on mac doesn't take keyboard input properly
-            # https://bitbucket.org/pygame/pygame/issues/203/window-does-not-get-focus-on-os-x-with
-            # port install py27-game as suggested fix
-            if event.key == pygame.K_LEFT: glTranslatef(-0.5,0,0)
-            if event.key == pygame.K_RIGHT: glTranslatef(0.5,0,0)
-            if event.key == pygame.K_UP: glTranslatef(0,1,0)
-            if event.key == pygame.K_DOWN: glTranslatef(0,-1,0)
-
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 4: glTranslatef(0,0,1.0)
-                if event.button == 5: glTranslatef(0,0,-1.0)
-
-        # glRotatef(1, 3, 1, 1)
+        glRotatef(1, 3, 1, 1)
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+        Cube2()
         Cube()
         pygame.display.flip()
         pygame.time.wait(10)
