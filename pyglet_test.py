58b2a3231ed67428c20f27e11e77ab7f7e75e49e
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2017-12-12 01:44:12 -0800

    pyglet test frameworked

---
+++ pyglet_test.py
@@ -0,0 +1,19 @@
+# https://pyglet.readthedocs.io/en/latest/programming_guide/quickstart.html
+import pyglet
+
+window = pyglet.window.Window()
+
+label = pyglet.text.Label('Hello, world',
+    font_name='Times New Roman',
+    font_size=36,
+    x=window.width//2,
+    y=window.height//2,
+    anchor_x='center',
+    anchor_y='center')
+
+@window.event
+def on_draw():
+    window.clear()
+    label.draw()
+
+pyglet.app.run()
