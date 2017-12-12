4ca85d648e0ede21d046e3b9d978a5ecd574a8df
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2017-12-12 15:23:12 -0800

    added screenshotting functionality

--- pyglet_test.py
+++ pyglet_test.py
@@ -7,29 +7,50 @@ from pyglet.window import key
 from pyglet.window import mouse
 from pyglet.gl import *
 
-from resources import vertices, colors, surfaces, edges
+from resources import vertices, colors, surfaces, edges, win_sz
 
-window = pyglet.window.Window(800, 600)
+window = pyglet.window.Window(*win_sz)
+box_x, box_y, box_z = 0, 0, 0
 
-# @window.event
-# def on_show():
-#     pyglet.gl.glClear(pyglet.gl.GL_COLOR_BUFFER_BIT | pyglet.gl.GL_DEPTH_BUFFER_BIT)
-#     # Set up projection matrix.
-#     pyglet.gl.glMatrixMode(pyglet.gl.GL_PROJECTION)
-#     pyglet.gl.glLoadIdentity()
-#     pyglet.gl.gluPerspective(
-#             46.0,
-#             float(cubeWindow.width)/cubeWindow.height,
-#             0.1,
-#             360)
-
+@window.event
+def on_show():
+    print("setting up window...")
+    pyglet.gl.glClear(
+            pyglet.gl.GL_COLOR_BUFFER_BIT | pyglet.gl.GL_DEPTH_BUFFER_BIT
+            )
+    # Set up projection matrix.
+    pyglet.gl.glMatrixMode(pyglet.gl.GL_PROJECTION)
+    pyglet.gl.glLoadIdentity()
+    pyglet.gl.gluPerspective(
+            46.0,
+            float(window.width)/window.height,
+            0.1,
+            360)
 
 @window.event
 def on_draw():
     window.clear()
 
 def update(dt):
-    pass
+    print("update loop")
+    #TODO move box here
+
+@window.event
+def on_key_press(symbol, modifiers):
+
+    global box_x, box_y, box_z
+    if symbol == key.LEFT:
+        pass
+    elif symbol == key.RIGHT:
+        pass
+    elif symbol == key.UP:
+        pass
+    elif symbol == key.DOWN:
+        pass
+    elif symbol == key.SPACE:
+        print("space, screenshotting")
+        pyglet.image.get_buffer_manager().get_color_buffer().save(
+                'screenshot.png')
 
 # window.push_handlers(pyglet.window.event.WindowEventLogger())
 
