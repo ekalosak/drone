4b7899b0a79c7c32fb24e5e43720a5d0919bcb74
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2017-12-12 02:19:49 -0800

    attempying to get pyglet opengl to work

--- opengl_test.py
+++ opengl_test.py
@@ -81,7 +81,9 @@ def main():
     display = (800,600)
     pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
     gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
-    glTranslatef(0.0,0.0, -5)
+    # glTranslatef(0.0,0.0, -5)
+    glTranslatef(0,0, -10)
+    glRotatef(25, 2, 1, 0)
 
     while True:
         for event in pygame.event.get():
@@ -89,7 +91,20 @@ def main():
                 pygame.quit()
                 quit()
 
-        glRotatef(1, 3, 1, 1)
+        if event.type == pygame.KEYDOWN:
+            # pygame on mac doesn't take keyboard input properly
+            # https://bitbucket.org/pygame/pygame/issues/203/window-does-not-get-focus-on-os-x-with
+            # port install py27-game as suggested fix
+            if event.key == pygame.K_LEFT: glTranslatef(-0.5,0,0)
+            if event.key == pygame.K_RIGHT: glTranslatef(0.5,0,0)
+            if event.key == pygame.K_UP: glTranslatef(0,1,0)
+            if event.key == pygame.K_DOWN: glTranslatef(0,-1,0)
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 4: glTranslatef(0,0,1.0)
+                if event.button == 5: glTranslatef(0,0,-1.0)
+
+        # glRotatef(1, 3, 1, 1)
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
         Cube()
         pygame.display.flip()
--- pyglet_test.py
+++ pyglet_test.py
@@ -1,29 +1,110 @@
 # https://pyglet.readthedocs.io/en/latest/programming_guide/quickstart.html
+# https://pyglet.readthedocs.io/en/latest/programming_guide/gl.html#guide-gl
+
 import pyglet
 from pyglet.window import key
+from pyglet.window import mouse
+from pyglet.gl import *
 
-window = pyglet.window.Window()
+# from opengl_test import vertices, edges, colors, surfaces
 
-label = pyglet.text.Label('Hello, world',
-    font_name='Times New Roman',
-    font_size=36,
-    x=window.width//2,
-    y=window.height//2,
-    anchor_x='center',
-    anchor_y='center')
+vertices= (
+        (1, -1, -1),
+        (1, 1, -1),
+        (-1, 1, -1),
+        (-1, -1, -1),
+        (1, -1, 1),
+        (1, 1, 1),
+        (-1, -1, 1),
+        (-1, 1, 1)
+    )
+
+edges = (
+        (0,1),
+        (0,3),
+        (0,4),
+        (2,1),
+        (2,3),
+        (2,7),
+        (6,3),
+        (6,4),
+        (6,7),
+        (5,1),
+        # (5,4),
+        (5,7)
+    )
+
+assert(all([all([e2 < len(vertices) for e2 in e1]) for e1 in edges]))
+
+colors = (
+        (1,0,0),
+        (0,1,0),
+        (0,0,1),
+        (0,1,0),
+        (1,1,1),
+        (0,1,1),
+        (1,0,0),
+        (0,1,0),
+        (0,0,1),
+        (1,0,0),
+        (1,1,1),
+        (0,1,1),
+    )
+
+surfaces = (
+        (0,1,2,3),
+        (3,2,7,6),
+        (6,7,5,4),
+        (4,5,1,0),
+        (1,5,7,2),
+        (4,0,3,6)
+    )
+
+
+window = pyglet.window.Window(800, 600)
+
+def Cube():
+    glBegin(GL_QUADS)
+    x = 1
+    for surface in surfaces:
+        # x = 0
+        for vertex in surface:
+            # x+=1
+            glColor3fv(*colors[x])
+            glVertex3fv(*vertices[vertex])
+    glEnd()
 
 @window.event
 def on_draw():
-    window.clear()
-    label.draw()
+    # glClear(GL_COLOR_BUFFER_BIT)
+    # glLoadIdentity()
+    # glBegin(GL_TRIANGLES)
+    # glVertex2f(0, 0)
+    # glVertex2f(window.width, 0)
+    # glVertex2f(window.width, window.height)
+    # glEnd()
 
-@window.event
-def on_key_press(symbol, modifiers):
-    if symbol == key.A:
-        print('The "A" key was pressed.')
-    elif symbol == key.LEFT:
-        print('The left arrow key was pressed.')
-    elif symbol == key.ENTER:
-        print('The enter key was pressed.')
+    glClear(GL_COLOR_BUFFER_BIT)
+    glMatrixMode(GL_MODELVIEW)
+    glBegin(GL_QUADS)
+    # for s in surfaces:
+    #     for v in s:
+    #         glVertex2f(*vertices[v])
+    for v in surfaces[0]:
+        glVertex2f(*vertices[v])
 
-pyglet.app.run()
+    # glBegin(GL_TRIANGLES)
+    # glVertex2i(*(300,300))
+    # glColor3f(*(0,1,0))
+    # glVertex2i(400,200)
+    # glColor3f(*(1,0,0))
+    # glVertex2i(200,200)
+    # glColor3f(*(0,0,1))
+    # glEnd()
+
+    # Cube()
+
+# window.push_handlers(pyglet.window.event.WindowEventLogger())
+
+if __name__ == '__main__':
+    pyglet.app.run()
