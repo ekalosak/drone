559df040107d6ae5cd68331660b466b2869fa7e5
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2017-12-12 01:46:41 -0800

    pyglet listening to keyboard

--- pyglet_test.py
+++ pyglet_test.py
@@ -1,5 +1,6 @@
 # https://pyglet.readthedocs.io/en/latest/programming_guide/quickstart.html
 import pyglet
+from pyglet.window import key
 
 window = pyglet.window.Window()
 
@@ -15,5 +16,14 @@ label = pyglet.text.Label('Hello, world',
 def on_draw():
     window.clear()
     label.draw()
+
+@window.event
+def on_key_press(symbol, modifiers):
+    if symbol == key.A:
+        print('The "A" key was pressed.')
+    elif symbol == key.LEFT:
+        print('The left arrow key was pressed.')
+    elif symbol == key.ENTER:
+        print('The enter key was pressed.')
 
 pyglet.app.run()
