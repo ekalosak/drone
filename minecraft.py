7aadf4211552d071582978414a5e42215e8471fe
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2017-12-12 22:25:51 -0800

    refactored; minecraft world rendering a small enclosed room

--- minecraft.py
+++ minecraft.py
@@ -161,38 +161,20 @@ class Model(object):
         """ Initialize the world by placing all the blocks.
 
         """
-        n = 80  # 1/2 width and height of world
+        n = 12  # 1/2 width and height of world
         s = 1  # step size
         y = 0  # initial y height
+        h = 3 # 1/2 height of ceiling
         for x in xrange(-n, n + 1, s):
             for z in xrange(-n, n + 1, s):
                 # create a layer stone an grass everywhere.
-                self.add_block((x, y - 2, z), GRASS, immediate=False)
-                self.add_block((x, y - 3, z), STONE, immediate=False)
+                self.add_block((x, y - h, z), STONE, immediate=False)
+                # add a ceiling
+                self.add_block((x, y + h, z), STONE, immediate=False)
                 if x in (-n, n) or z in (-n, n):
                     # create outer walls.
-                    for dy in xrange(-2, 3):
+                    for dy in xrange(-(h-1), h):
                         self.add_block((x, y + dy, z), STONE, immediate=False)
-
-        # # generate the hills randomly
-        # o = n - 10
-        # for _ in xrange(120):
-        #     a = random.randint(-o, o)  # x position of the hill
-        #     b = random.randint(-o, o)  # z position of the hill
-        #     c = -1  # base of the hill
-        #     h = random.randint(1, 6)  # height of the hill
-        #     s = random.randint(4, 8)  # 2 * s is the side length of the hill
-        #     d = 1  # how quickly to taper off the hills
-        #     t = random.choice([GRASS, SAND, BRICK])
-        #     for y in xrange(c, c + h):
-        #         for x in xrange(a - s, a + s + 1):
-        #             for z in xrange(b - s, b + s + 1):
-        #                 if (x - a) ** 2 + (z - b) ** 2 > (s + 1) ** 2:
-        #                     continue
-        #                 if (x - 0) ** 2 + (z - 0) ** 2 < 5 ** 2:
-        #                     continue
-        #                 self.add_block((x, y, z), t, immediate=False)
-        #         s -= d  # decrement side lenth so hills taper off
 
     def hit_test(self, position, vector, max_distance=8):
         """ Line of sight search from current position. If a block is
@@ -442,7 +424,7 @@ class Window(pyglet.window.Window):
         self.exclusive = False
 
         # When flying gravity has no effect and speed is increased.
-        self.flying = False
+        self.flying = True
 
         # Strafing is moving lateral to the direction you are facing,
         # e.g. moving to the left or right while continuing to face forward.
